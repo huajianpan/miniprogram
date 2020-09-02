2ef5f9215d064f345a8563d414c5808fdd98c9b4
潘潘 <97274247@qq.com>
2020-09-03 00:42:52 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+  }
+})
