1603128e2e9610dbfcd12f7b352e4f1d72e25f75
潘潘 <97274247@qq.com>
2020-09-03 01:30:23 +0800

    todos插入数据

---
+++ miniprogram/pages/addTodo/addTodo.js
@@ -0,0 +1,82 @@
+// pages/addTodo/addTodo.js
+const db=wx.cloud.database()
+const todos=db.collection('todos')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  onSubmit:function(event){
+    console.log(event)
+    todos.add({
+      data:{
+        title:event.detail.value.title
+      }
+    }).then(res=>{
+      console.log(res)
+      wx.showToast({
+        title: '已提交',
+        icon:'success'
+      })
+    })
+  }
+})
